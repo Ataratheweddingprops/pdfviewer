0c31b64f3acd75ce1b405ee61d39e10a39f94db5
Dhruvil Jariwala <jariwaladhruvil@gmail.com>
2023-11-04 03:21:36 +0530

    webp optimized v1.2

--- script.bat
+++ script.bat
@@ -15,18 +15,18 @@ set "output_directory=%script_dir%images"
 
 :: Ensure the output directory exists; create it if not
 if not exist "%output_directory%" mkdir "%output_directory%"
-pdftopng
+@REM pdftopng
 
-:: Loop through all PDF files in the "pdfs" folder and convert each one to PNG
-for %%i in ("%input_pdf_directory%\*.pdf") do (
+@REM :: Loop through all PDF files in the "pdfs" folder and convert each one to PNG
+@REM for %%i in ("%input_pdf_directory%\*.pdf") do (
 
-    set "pdf_file=%%~nxi"
-    echo Converting "%%~nxi" to PNG...
-    echo %pdf_file%
+@REM     set "pdf_file=%%~nxi"
+@REM     echo Converting "%%~nxi" to PNG...
+@REM     echo %pdf_file%
 
-    pdftopng.exe -r 200 "%input_pdf_directory%\%pdf_file%" "%output_directory%\%%~nxi_page"
+@REM     pdftopng.exe -r 102 "%input_pdf_directory%\%pdf_file%" "%output_directory%\%%~nxi_page"
 
-)
+@REM )
 
 :: Loop through all PNG files in the "images" folder and convert each one to WebP
 for %%f in ("%output_directory%\*.png") do (
@@ -39,21 +39,21 @@ for %%f in ("%output_directory%\*.png") do (
     del "%%f"
 )
 
-echo All PDFs in the "pdfs" folder converted to PNG.
+@REM echo All PDFs in the "pdfs" folder converted to PNG.
 
-@echo off
+@REM @echo off
 
-:: Add all changes to the staging area
-git add .
+@REM :: Add all changes to the staging area
+@REM git add .
 
-:: Prompt the user for a commit message
-set /p commit_message="Enter your commit message: "
+@REM :: Prompt the user for a commit message
+@REM set /p commit_message="Enter your commit message: "
 
-:: Commit the changes with the provided commit message
-git commit -m "%commit_message%"
+@REM :: Commit the changes with the provided commit message
+@REM git commit -m "%commit_message%"
 
-:: Push the changes to the remote repository (replace "origin" and "master" with your remote and branch)
-git push origin main
+@REM :: Push the changes to the remote repository (replace "origin" and "master" with your remote and branch)
+@REM git push origin main
 
 :: Pause to keep the command prompt window open for feedback
 pause
